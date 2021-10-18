eb5162a799713e49862af63d5dd7c68636a1ae3b
Wole Ajewole <wolemercy@gmail.com>
2021-10-18 14:51:09 +0100

    updated workflows

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -1,43 +1,38 @@
-# from django.test import TestCase
-# from api.models import Book
-# from django.contrib.auth.models import User
+from django.test import TestCase
+from api.models import Book
+from django.contrib.auth.models import User
 
 
-# class BookTestCase(TestCase):
+class BookTestCase(TestCase):
 
-#     def setUp(self) -> None:
-#         reader = User.objects.create_user(username='Phil')
+    def setUp(self) -> None:
+        reader = User.objects.create_user(username='Phil')
 
-#         Book.objects.create(title='Brave New World',
-#                             author='Aldous Huxley',
-#                             currentPage=15,
-#                             totalPages = 120,
-#                             reader = reader
-#                             )
-#         Book.objects.create(title='Paradise Lost',
-#                             author='John Milton',
-#                             currentPage=28,
-#                             totalPages = 95,
-#                             reader = reader
-#                             )
+        Book.objects.create(title='Brave New World',
+                            author='Aldous Huxley',
+                            currentPage=15,
+                            totalPages = 120,
+                            reader = reader
+                            )
+        Book.objects.create(title='Paradise Lost',
+                            author='John Milton',
+                            currentPage=28,
+                            totalPages = 95,
+                            reader = reader
+                            )
     
-#     def test_author_name_is_correct(self): 
-#         """
-#         Ensure books have appropriate author names
-#         """
-#         book1 = Book.objects.get(title='Brave New World')
+    def test_author_name_is_correct(self): 
+        """
+        Ensure books have appropriate author names
+        """
+        book1 = Book.objects.get(title='Brave New World')
 
-#         self.assertEqual(book1.author, 'Aldous Huxley')
+        self.assertEqual(book1.author, 'Aldous Huxley')
 
-#     def test_author_name_is_wrong(self): 
-#         """
-#         Ensure books with inappropriate author names are flagged
-#         """
-#         book2 = Book.objects.get(title='Paradise Lost')
+    def test_author_name_is_wrong(self): 
+        """
+        Ensure books with inappropriate author names are flagged
+        """
+        book2 = Book.objects.get(title='Paradise Lost')
 
-#         self.assertNotEqual(book2.author, 'John Boyega')
-
-
-
-
-        
+        self.assertNotEqual(book2.author, 'John Boyega')
